17ae6a0e8254c81d9885340bc04f9bcf270d7ed2
natsu731no <na0731.k@gmail.com>
2022-05-27 14:46:57 +0900

    [update]init.sqlの初期sqlを消した

--- db/mysql_init/init.sql
+++ db/mysql_init/init.sql
@@ -1,28 +1,6 @@
 CREATE DATABASE IF NOT EXISTS earlGrey;
 use earlGrey;
 
--- CREATE TABLE user (
---     user_id INT(8) NOT NULL,
---     user_name VARCHAR(200) NOT NULL,
---     password varchar(270) NOT  NULL,
---     mail varchar(30) unique
---     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ALTER TABLE user
---   ADD PRIMARY KEY (`user_id`);
-
-
-
-
-
--- ALTER TABLE user
---   MODIFY user_id int(11) AUTO_INCREMENT,AUTO_INCREMENT=1;
-
--- INSERT INTO user (user_name, password, mail)
---         VALUES ("test一郎",
---                 "paspaspas",
---                 "2example@gmail.com");
-
 --何限目の情報を格納している
 CREATE TABLE timer(
 	timeNo char(3) not null,
