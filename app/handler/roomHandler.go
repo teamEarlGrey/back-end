6a5faa886ae21e5d3b1686ffedb9d88b98de9df8
Kantaro0829 <eshangwan@gmail.com>
2022-06-11 14:57:19 +0900

    [add] 通常授業の教室使用状況のJsonを作る関数を作成

--- app/handler/roomHandler.go
+++ app/handler/roomHandler.go
@@ -24,6 +24,7 @@ func GetRoomInfo(c *gin.Context) {
 	roomNum, _ := strconv.ParseInt(roomNumStr, 10, 16)
 	//roomNumberの上二桁だけ切り取り
 	buildingNumAndFloor := roomNum / 100
+	fmt.Println(buildingNumAndFloor)
 
 	db := infra.DBInit()
 	teachers := []model.Teacher{}
@@ -64,5 +65,50 @@ func GetRoomInfo(c *gin.Context) {
 		fmt.Printf("%v, %v, %v, %v\n", v.RoomNo, v.TimeNo, v.TeacherName, v.SubjectName)
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": buildingNumAndFloor})
+	jsonToResponse := createRoomInfoJson(roomResults)
+
+	//c.JSON(http.StatusOK, gin.H{"message": buildingNumAndFloor})
+	c.JSON(http.StatusOK, jsonToResponse)
+}
+
+type Class struct {
+	TimeNo      string
+	TeacherName string
+	SubjectName string
+}
+
+func createRoomInfoJson(roomInfos []model.RoomResult) map[uint16][]Class {
+	//各教室の状況を格納するJson配列を作成する
+	var currentRoomNo uint16                  //同じ教室番号を配列に分割するために判断する変数
+	eachRoomInfos := make(map[uint16][]Class) //最終的に出力したいJsonの型宣言
+	roomInfo := []Class{}
+
+	for i, v := range roomInfos {
+		fmt.Printf("%v, %v, %v, %v\n", v.RoomNo, v.TimeNo, v.TeacherName, v.SubjectName)
+		if i == 0 {
+			//ループの最初は変数currentRoomに代入
+			currentRoomNo = v.RoomNo
+		}
+
+		if currentRoomNo != v.RoomNo {
+			//以前の教室番号と違う教室番号の場合新しい連想配列を作る
+			eachRoomInfos[currentRoomNo] = roomInfo
+			roomInfo = []Class{} //各教室1~5限情報をを格納する配列の初期化
+			currentRoomNo = v.RoomNo
+		}
+
+		//各教室の1〜5限目の情報を格納する配列に値を入れる
+		roomInfo = append(roomInfo, Class{
+			TimeNo:      v.TimeNo,
+			TeacherName: v.TeacherName,
+			SubjectName: v.SubjectName,
+		})
+
+	}
+	//最後だけfor文が回らないので
+	eachRoomInfos[currentRoomNo] = roomInfo
+	fmt.Println("------------------出来上がったJson---------------------")
+	fmt.Println(eachRoomInfos)
+	return eachRoomInfos
+
 }
